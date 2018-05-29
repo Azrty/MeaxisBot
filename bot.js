dfecd57997a1de9c851cc1ec046387353ca1756d
Meaxis <37759268+Meaxis@users.noreply.github.com>
2018-05-29 20:52:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Oof') {
+        message.reply('Uuh');
+    }
+}};
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
