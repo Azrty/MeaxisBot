d120a91bd2a47678966c0ab589f828c9f51e5c1c
Meaxis <37759268+Meaxis@users.noreply.github.com>
2018-05-29 21:33:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,5 +34,11 @@ client.on('message', message => {
   	}
 });
 
+
+client.on('message', message => {
+    if (message.content === 'Ok Google, leaf.') {
+    	message.reply("L'élu a été invoqué. Préparez les Smots.");
+  	}
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
